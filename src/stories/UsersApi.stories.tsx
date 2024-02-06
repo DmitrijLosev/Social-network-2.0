2cd0aab9f23b958ce74b95eea61cc04ae2c17052
DmitrijLosev <dmitrijslosevs@inbox.lv>
2024-02-06 09:28:52 +0200

    auth, follow, unfollow

---
+++ src/stories/UsersApi.stories.tsx
@@ -0,0 +1,68 @@
+import {useEffect, useState} from "react";
+import {usersApi} from "../api/api-users";
+
+
+
+
+export default {
+    title: "API/UsersApi",
+};
+
+
+//SYNTAX SUGAR!!!!!!!!!!!!!!!!!!!!!!!!!
+/*type Story = StoryObj<typeof ControlledAccordion>;
+
+export const FirstStory: Story = {
+    args: {
+        titleValue:"CollapsedAccordion",
+        collapsed:true,
+    },
+};*/
+
+/*const onChangeHandler = action("onChange Accordion")
+const callback = action("some item was clicked")*/
+export const GetUsersApi = () => {
+    const [state,setState]=useState<any>()
+    useEffect(() => {
+
+        (async () => {
+                let data = await usersApi.getUsers(100,2)
+                setState(data)
+            }
+        )()
+
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+};
+
+export const FollowUserApi = () => {
+    const [state,setState]=useState<any>()
+    useEffect(() => {
+
+        (async () => {
+                let data = await usersApi.followUser(2)
+                setState(data)
+            }
+        )()
+
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+};
+export const UnfollowUserApi = () => {
+    const [state,setState]=useState<any>()
+    useEffect(() => {
+
+        (async () => {
+                let data = await usersApi.unfollowUser(2)
+                setState(data)
+            }
+        )()
+
+    }, [])
+
+    return <div>{JSON.stringify(state)}</div>
+};
+
+
